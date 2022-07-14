6d2688fef0070882d5360315c0d6de3b135973b3
Daniel-Andarge <andargedaniel90@gmail.com>
2022-07-13 17:48:01 -0700

    Implement the nop opcode

---
+++ stack_nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * stack_nop - doesnt do anything
+ * @stack: unused
+ * @line_number: unused
+ */
+void stack_nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
